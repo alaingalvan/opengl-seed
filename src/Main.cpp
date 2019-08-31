77c0e82b2e170f7e7dab45cb3f1e9b06337528bf
Alain Galvan <hi@alain.xyz>
2019-08-31 19:43:17 -0400

    Fixes to CrossWindow, GLM

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,45 +1,52 @@
+#include "CrossWindow/Main/Main.h"
 #include "CrossWindow/CrossWindow.h"
-#include "glad.h"
+#include "glad/glad.h"
 #include "CrossWindow/Graphics.h"
 #include "glm/glm.hpp"
 
-void xmain(int argc, char** argv)
+
+void xmain(int argc, const char** argv)
 {
-    // Configure our Window
-    xwin::WindowDesc wdesc;
-    wdesc.title = "OpenGL Seed";
-    wdesc.width = 1280;
-    wdesc.height = 720;
+	// Configure our Window
+	xwin::WindowDesc wdesc;
+	wdesc.title = "OpenGL Seed";
+	wdesc.name = "MainWindow";
+	wdesc.visible = true;
+	wdesc.width = 1280;
+	wdesc.height = 720;
+	wdesc.fullscreen = false;
 
-    xwin::Window window;
-    xwin::EventQueue eventQueue;
-    
-    if(window.create(wdesc, eventQueue))
-    { return; };
+	xwin::Window window;
+	xwin::EventQueue eventQueue;
 
-    // 🏁 Engine loop
-    bool isRunning = true;
-    while (isRunning)
-    {
-        bool shouldRender = true;
+	if (!window.create(wdesc, eventQueue))
+	{
+		return;
+	};
 
-        // ♻️ Update the event queue
-        eventQueue.update();
+	// 🏁 Engine loop
+	bool isRunning = true;
+	while (isRunning)
+	{
+		bool shouldRender = true;
 
-        // 🎈 Iterate through that queue:
-        while (!eventQueue.empty())
-        {
-            //Update Events
-            const xwin::Event& event = eventQueue.front();
+		// ♻️ Update the event queue
+		eventQueue.update();
 
-            if (event.type == xwin::EventType::Close)
-            {
-                window.close();
-                shouldRender = false;
-                isRunning = false;
-            }
+		// 🎈 Iterate through that queue:
+		while (!eventQueue.empty())
+		{
+			//Update Events
+			const xwin::Event& event = eventQueue.front();
 
-            eventQueue.pop();
-        }
-    }
+			if (event.type == xwin::EventType::Close)
+			{
+				window.close();
+				shouldRender = false;
+				isRunning = false;
+			}
+
+			eventQueue.pop();
+		}
+	}
 }
